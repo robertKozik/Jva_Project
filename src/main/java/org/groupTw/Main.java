3da2e7871a3f2c4fbdc636cfa511dd4f16af4aca
uniQueSense <uniQueSense@users.noreply.github.com>
2020-06-27 11:29:26 +0200

    Add commit!

--- src/main/java/org/groupTw/Main.java
+++ src/main/java/org/groupTw/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args){
 
         AppFrame app = new AppFrame();
-        app.setVisible(true);
-        // kom
+        app.setVisible(true)
     }
 }
